742f2541a85b1e4fd77971c54411bdffc7f0221f
Stef Harrington-Palmer <stef.hp@cloudreach.com>
2023-12-01 11:27:09 +0000

    feat: part 2

--- day1/wheeeee.ps1
+++ day1/wheeeee.ps1
@@ -1,6 +1,15 @@
 $bar = Get-Content -Path "./input"
 try {
     foreach ($foo in $bar) {
+        $foo = $foo -replace 'one', 'o1e'
+        $foo = $foo -replace 'two', 't2o'
+        $foo = $foo -replace 'three', 't3e'
+        $foo = $foo -replace 'four', 'f4r'
+        $foo = $foo -replace 'five', 'f5e'
+        $foo = $foo -replace 'six', 's6x'
+        $foo = $foo -replace 'seven', 's7n'
+        $foo = $foo -replace 'eight', 'e8t'
+        $foo = $foo -replace 'nine', 'n9e'
         $foo = $foo -replace '[a-zA-Z]', ''
         [int]$smunge = [string]$foo[0] + [string]$foo[-1]
         [int]$running_total = $running_total + $smunge
