9cecc3cd2c465f1890bb720ccb684a9f463b1343
Stef Harrington-Palmer <stef.hp@cloudreach.com>
2023-12-02 19:49:05 +0000

    feat: part 2

--- day2/turtledoves.ps1
+++ day2/turtledoves.ps1
@@ -1,7 +1,3 @@
-$limit_r = 12
-$limit_g = 13
-$limit_b = 14
-
 $bar = Get-Content -Path "./input"
 
 foreach ($foo in $bar) {
@@ -11,21 +7,44 @@ foreach ($foo in $bar) {
     $failed = 0
     foreach ($split in $pull_split) {
         $colour_split = $split.Split(",")
+        $blues_highest = 0
+        $reds_highest = 0
+        $greens_highest = 0
         foreach ($colour in $colour_split) {
-            if ($colour.Contains("blue") -and ([int]($colour -replace '[a-zA-Z]').Replace(" ", "")) -gt $limit_b ) {
+            if ($colour.Contains("blue") ) {
+                $blues = ([int]($colour -replace '[a-zA-Z]').Replace(" ", ""))
+                if ($blues_highest -lt $blues) {
+                    $blues_highest = $blues
+                }
                 $failed = $failed + 1
             }
-            elseif ($colour.Contains("red") -and ([int]($colour -replace '[a-zA-Z]').Replace(" ", "")) -gt $limit_r ) {
+            elseif ($colour.Contains("red")) {
+                $reds = ([int]($colour -replace '[a-zA-Z]').Replace(" ", ""))
+                if ($reds_highest -lt $reds) {
+                    $reds_highest = $reds
+                }
                 $failed = $failed + 1
             }
-            elseif ($colour.Contains("green") -and ([int]($colour -replace '[a-zA-Z]').Replace(" ", "")) -gt $limit_g ) {
+            elseif ($colour.Contains("green")) {
+                $greens = ([int]($colour -replace '[a-zA-Z]').Replace(" ", ""))
+                if ($greens_highest -lt $greens) {
+                    $greens_highest = $greens
+                }
                 $failed = $failed + 1
             }
         }
+        if ($greeny_highest -lt $greens_highest) {
+            $greeny_highest = $greens_highest
+        }
+        if ($redy_highest -lt $reds_highest) {
+            $redy_highest = $reds_highest
+        }
+        if ($bluey_highest -lt $blues_highest) {
+            $bluey_highest = $blues_highest
+        }
     }
-    if ($failed -eq 0) {
-        Write-Host "$game_number passed ze tests"
-        [int]$sum = $sum + $game_number
-        Write-Host "Sum of successes is $sum"
-    }
+    $christmas_power = $greeny_highest * $redy_highest * $bluey_highest
+    Write-Host "G: $game_number R:$redy_highest G:$greeny_highest B:$bluey_highest P:$christmas_power"
+    $sum_christmas_power = $sum_christmas_power + $christmas_power
 }
+Write-Host $sum_christmas_power
